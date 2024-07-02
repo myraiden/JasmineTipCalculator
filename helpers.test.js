3d8cb21f3e075e3ce1916d6fe4dc177b100d82d2
myraiden <klj3604@gmail.com>
2024-07-02 00:10:04 -0400

    completed updates of jasmine testing

--- helpers.test.js
+++ helpers.test.js
@@ -1,22 +1,35 @@
 describe("Helpers test (with setup and tear-down)", function() {
-    beforeEach(function () {
+    beforeAll(function () {
       // initialization logic
       // needs values for allPayments object
+      billAmtInput.value=35;
+      tipAmtInput.value=15;
+      submitPaymentInfo();
+      billAmtInput.value=15;
+      tipAmtInput.value=5;
+      submitPaymentInfo();
       
     });
   
     it('should add all payments together from allPayments()', function () {
       //test sumPaymentTotal()
-  
+      expect(sumPaymentTotal('billAmt')).toEqual(50);
+      
     });
   
     it('should convert the tip amount into a percent from the bill', function () {
         //test calculateTipPercent()
+        let billAmt=sumPaymentTotal('billAmt');
+        let tipAmt=sumPaymentTotal('tipAmt')
+        expect(calculateTipPercent(billAmt, tipAmt)).toEqual(40);
     
       });
 
-    afterEach(function() {
+    afterAll(function() {
       // teardown logic
-
+      billAmtInput.value='';
+      tipAmtInput.value='';
+      allPayments={};
+      updateSummary();
     });
   });
--- payments.test.js
+++ payments.test.js
@@ -1,18 +1,41 @@
 describe("Payments test (with setup and tear-down)", function() {
-    beforeEach(function () {
+    beforeAll(function () {
       // initialization logic
       // needs input values
-      
+      billAmtInput.value = 125.65;
+      tipAmtInput.value = 28.90;
+
     });
   
+    it('should return valid for positive values', function () {
+      // createCurPayment() will return undefined with negative or empty inputs
+        // positive billAmt is required but tip can be 0
+
+        submitPaymentInfo();
+        expect(Object.keys(allPayments).length).toEqual(1);
+        expect(allPayments.payment1.tipPercent).toBeCloseTo(23);
+  
+    });
+
     it('should return undefined for a negative or empty input', function () {
       // createCurPayment() will return undefined with negative or empty inputs
         // positive billAmt is required but tip can be 0
+        billAmtInput.value='';
+        submitPaymentInfo();
+        expect(Object.keys(allServers).length).toEqual(0);
+
+        billAmtInput.value=0;
+        submitPaymentInfo();
+        expect(Object.keys(allServers).length).toEqual(0);
   
     });
 
-    afterEach(function() {
+    afterAll(function() {
       // teardown logic
-
+      billAmtInput.value='';
+      tipAmtInput.value='';
+      allPayments={};
+      paymentTbody.remove();
+      updateSummary();
     });
   });
